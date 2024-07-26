bf3bc14ff6e034f260e23629cf1fae90ffe5a684
Amber Torrise <amber.torrise@broadcom.com>
2024-07-26 13:22:12 -0400

    maybe????
    Signed-off-by: Amber Torrise <amber.torrise@broadcom.com>

--- .github/updateReleaseMarkdown.js
+++ .github/updateReleaseMarkdown.js
@@ -25,7 +25,7 @@ function fetchCliTeam(url) {
       // The whole response has been received
       res.on('end', () => {
         // Extract only the CLI contributors section
-        const cliSectionMatch = data.match(/## Zowe CLI Squad[\s\S]*?(?=##|$)/);
+        const cliSectionMatch = data.match(/### Zowe CLI Squad[\s\S]*?(?=###|$)/);
         const cliSection = cliSectionMatch ? cliSectionMatch[0] : '';
         resolve(cliSection);
       });
@@ -44,8 +44,9 @@ function updateCliTeamInMd(cliTeam) {
       return;
     }
 
-    // Replace the existing CLI team section with the new one
-    const updatedData = data.replace(/## Zowe CLI Squad[\s\S]*?(?=##|$)/, cliTeam);
+    // Regular expression to match the "Zowe CLI Squad" section
+    const cliSquadRegex = /## Zowe CLI Squad[\s\S]*?(?=##|$)/;
+    const updatedData = data.replace(cliSquadRegex, cliTeam);
 
     fs.writeFile(mdFilePath, updatedData, 'utf8', (err) => {
       if (err) {
